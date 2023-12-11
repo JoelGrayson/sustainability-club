1b9c0d13a6b4a1ac6c67fdea534c25e6223dd70d
Joel Grayson <joel@joelgrayson.com>
2023-12-11 14:14:26 -0500

    starting with markdown

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
 /** @type {import('next').NextConfig} */
+const withMDX = require('@next/mdx')()
 
 const jredirects=require('./jredirects/dist');
-
-module.exports={
+ 
+const nextConfig={
+    // Configure `pageExtensions` to include MDX files
+    pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+    // Optionally, add any other Next.js config below
     reactStrictMode: true,
     swcMinify: true,
-    redirects: jredirects
-};
+    redirects: jredirects,
+}
+ 
+module.exports = withMDX(nextConfig)
